03bd940ca1c74a796b05252e9937718e07336ac8
Edwin Calma Jr. <edwincalmajr@gmail.com>
2024-02-06 17:23:44 +0800

    2nd Commit - Node.js

--- intern-nodejs/app.js
+++ intern-nodejs/app.js
@@ -1,6 +1,20 @@
-const http = require("http");
-const routes = require("./routes")
+const express = require('express');
+const bodyparser = require('body-parser');
+const path = require('path');
 
-const server = http.createServer(routes);
+const app = express();
 
-server.listen(3000);
+const adminroutes = require('./routes/admin');
+const shoproutes = require('./routes/shop');
+
+app.use(bodyparser.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(shoproutes);
+app.use('/admin', adminroutes);
+
+app.use ((req, res, next) => {
+    res.sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+app.listen(3000);
